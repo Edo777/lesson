e4f3536e07458b2feb565ff4d2b550a56dd8378e
edo777 <edgar.khachatryan95@mail.ru>
2018-02-28 16:45:54 +0300

    add POST /todos request from postman

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { mongoose } = require('./db/mongoose');
+const { User } = require("./models/user");
+const { Todo } = require("./models/todo");
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended : true
+}));
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save()
+        .then((doc) => {
+            res.send(doc)
+        }, (e) => {
+            res.status(400).send(e)
+        })
+})
+
+app.listen(3000, () => {
+    console.log("Starting on port 3000");
+})
