b347d1c958bc33f1a4dca3daba06c3b44ea548c6
edo777 <edgar.khachatryan95@mail.ru>
2018-03-02 13:52:00 +0300

    delete /:id lesson 82

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const { ObjectID } = require("mongodb");
 const { mongoose } = require('./db/mongoose');
 const { User } = require("./models/user");
 const { Todo } = require("./models/todo");
 var app = express();
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -16,8 +18,8 @@ app.post('/todos', (req, res) => {
         text: req.body.text
     });
     todo.save()
-        .then((doc) => {
-            res.send(doc)
+        .then((todos) => {
+            res.send({todos})
         }, (e) => {
             res.status(400).send(e)
         })
@@ -25,10 +27,46 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     Todo.find({}).then((result)=>{
-        res.send(result)
+        res.send({result})
     }).catch((err)=>{
         res.status(400).send(err);
     })
+})
+
+app.get("/todos/:id", (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send("invalid user")
+    }
+
+    Todo.findById(id)
+        .then((todo) => {
+           if(!todo) {
+               return res.status(404).send()
+           }
+           res.send({todo})
+        }, (err) => {
+            res.status(400).send(err);
+        })
+
+})
+
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    console.log(id)
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send()
+    }
+    Todo.findByIdAndRemove(id)
+        .then((deleted) => {
+            if(!deleted){
+                return res.status(400).send()
+            }
+            res.send({deleted})
+        })
+        .catch((error) => {
+            res.status(400).send();
+        })
 })
 
 app.listen(3000, () => {
